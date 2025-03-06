348468b9eac4cc50f6fff83bdfba3cfe5832a0b8
Christine Ong <christine.ong@sviisca.com>
2025-03-06 15:14:04 +0800

    Update Cypress test cases

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,14 +1,16 @@
 describe('home page', () => {
-  it('header is correct', () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000")
+  })
+
+  it('header is correct', () => {
     cy.get("[data-test='hero-heading']")
     .should("exist")
     .contains("Testing Next.js Applications with Cypress")
     
   })
 
-  it.only("correct homepage features", () => {
-    cy.visit("http://localhost:3000")
+  it("correct homepage features", () => {
     cy.get("dt").eq(0).contains("4 Courses")
     cy.get("dt").eq(1).contains("25+ Lessons")
     cy.get("dt").eq(2).contains("Free and Open Source")
